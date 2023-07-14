a55f3f376aa00f7cb2431567cc6da95d75eccf49
Denlie <denliehoo@gmail.com>
2023-07-14 21:09:57 +0800

    Added routes for frontend

---
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+// import classes from "./Dashboard.module.css";
+
+const Dashboard = () => {
+  return <div>This is dashboard</div>;
+};
+
+export default Dashboard;
---
+++ client/src/pages/manageSubscription/ManageSubcriptionExternal.tsx
@@ -0,0 +1,7 @@
+// import classes from "./ManageSubscriptionExternal.module.css";
+
+const ManageSubscriptionExternal = () => {
+  return <div>Thisis manage....</div>;
+};
+
+export default ManageSubscriptionExternal;
